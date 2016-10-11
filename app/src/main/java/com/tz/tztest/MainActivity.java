258091d5f832d42fa3fa7f5cc081106587568072
NishinoNanasemaru <932064280@qq.com>
2016-10-12 02:45:20 +0800

    1

--- app/src/main/java/com/tz/tztest/MainActivity.java
+++ app/src/main/java/com/tz/tztest/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.Random;
+
 import static com.tz.tztest.R.id.bt;
 import static com.tz.tztest.R.id.ll;
 
@@ -34,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
     public void doClick(View view) {
         switch (view.getId()) {
             case R.id.bt:
-                for (int i = 0;i<5;i++){
-                    nishinoNanase();
+                Random random = new Random();
+                int i = random.nextInt(5);
+                for (; i < 5; i++) {
+                    nishinoNanase(i);
                 }
                 break;
             default:
@@ -43,9 +47,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void nishinoNanase() {
+    private void nishinoNanase(int i) {
         mLinearLayout.addView(mTextView);
-        String name = "西野七濑";
+        String name = "西野七濑"+i;
         mTextView.setText(name);
         mTextView.setGravity(Gravity.CENTER);
         mTextView.setTextSize(48);
